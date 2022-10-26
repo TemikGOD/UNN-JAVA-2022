fd3d56d15497fcb2516a9af72226d9dbc8024385
wsemnazlo <92176898+wsemnazlo@users.noreply.github.com>
2022-10-27 02:12:17 +0300

    Delete Projects/Project-Two directory

--- Projects/Project-Two/src/IWatch.java
+++
@@ -1,8 +0,0 @@
-public interface IWatch {
-    enum Arrow { Hours, Minutes, Seconds }
-    int getPrice();
-    String getBrand();
-    int getHours();
-    int getMinutes();
-    void setTime(Arrow type, int value);
-}
